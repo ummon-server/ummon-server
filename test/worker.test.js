4b3687400781d933346c7efaa04e3fd75781c3ba
Matt McManus <mattmcmanus@gmail.com>
2013-06-07 17:07:15 -0400

    Fixed the worker test
    For some reason, when I ran tap test it would expect an additional
    argument than when I simply ran node test/worker.test.js. I think it
    has something to do with the spawned workers. This fixed it

--- test/worker.test.js
+++ test/worker.test.js
@@ -2,33 +2,6 @@
 
 var test = require("tap").test;
 
-// var thingie, superEasy;
-
-// test("make sure the thingie is a thing", function (t) {
-//   t.equal(thingie, "thing", "thingie should be thing");
-//   t.type(thingie, "string", "type of thingie is string");
-//   t.ok(true, "this is always true");
-//   t.notOk(false, "this is never true");
-//   t.test("a child test", function (t) {
-//     t.equal(this, superEasy, "right!?");
-//     t.similar(7, 2, "ever notice 7 is kinda like 2?", {todo: true});
-//     t.test("so skippable", {skip: true}, function (t) {
-//       t.plan(1); // only one test in this block
-//       t.ok(true, "but when the flag changes, it'll pass");
-//       // no need to end, since we had a plan.
-//     });
-//     t.end();
-//   });
-//   t.ok(99, "can also skip individual assertions", {skip: true});
-//   // end lets it know it's over.
-//   t.end();
-// });
-// test("another one", function (t) {
-//   t.plan(1);
-//   t.ok(true, "It's ok to plan, and also end.  Watch.");
-//   t.end(); // but it must match the plan!
-// });
-
 var ummon = require('..').create();
 var worker = require('../lib/worker.js');
 var run = require('../lib/run.js');
@@ -50,5 +23,15 @@ test('Test successfully running code with a worker', function(t){
 
   ummon.dispatcher.once('worker.complete', function(run){
     t.equal(run.exitCode, 0, 'The task runs and returns it\'s exit code of 0');
+    t.end();
   });
+});
+
+
+// The test doesn't exit because of something the worker is doing. 
+test('teardown', function(t){
+  setImmediate(function() {
+    process.exit();
+  });
+  t.end();
 });
