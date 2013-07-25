8318a00825e8bcce02632ee2dae345ceacaf0895
Matt McManus <mattmcmanus@gmail.com>
2013-07-25 17:24:30 -0400

    Wildcard triggers are setup. Closes #49

---
+++ test/ummon.task-ids.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var test = require("tap").test;
+var moment = require("moment");
+var async = require("async");
+
+var ummon = require('..')();
+ummon.pause = true;
+
+// Stub in some tasks
+ummon.tasks = {
+  'ummon.one': {"collection":"ummon"},
+  'ummon.two': {"collection":"ummon"},
+  'ummon.three': {"collection":"ummon"},
+  'canada.four': {"collection":"canada"}
+}
+
+test('Return the proper task id', function(t){
+  t.plan(3);
+  t.equal(ummon.getTaskId('ummon.one'), 'ummon.one', "Return the proper task id");
+  t.equal(ummon.getTaskId('one'), 'ummon.one', "Return the proper task id");
+  t.equal(ummon.getTaskId('oneone'), false, "Yell if there isn't a task with that id");
+})
+
+test('Return an array of task ids', function(t) {
+  t.plan(4);
+  t.similar(ummon.getTaskIds(), ['ummon.one','ummon.two','ummon.three','canada.four'], 'getTaskIds() returns all of the tasks');
+  t.similar(ummon.getTaskIds('*'), ['ummon.one','ummon.two','ummon.three','canada.four'], 'getTaskIds(*) returns all of the tasks');
+  t.similar(ummon.getTaskIds('ummon.*'), ['ummon.one','ummon.two','ummon.three'], 'getTaskIds("ummon.*") returns all of the ummon tasks');
+  t.similar(ummon.getTaskIds('ummon.one'), ['ummon.one'], 'getTaskIds("ummon.one") returns all of the ummon tasks');
+});
+
+test('Return an array of collection names', function(t){
+  t.plan(1);
+  t.similar(ummon.getCollections(), ['ummon','canada'], "There should be two collection names")
+})
---
+++ test/ummon.wildcard-deps.test..js
@@ -0,0 +1,27 @@
+'use strict';
+
+var test = require("tap").test;
+var moment = require("moment");
+var async = require("async");
+
+var ummon = require('..')();
+ummon.pause = true;
+
+test('Create a tasks with a wildcard trigger', function(t) {
+  t.plan(3);
+
+  // Create a bunch of dummy tasks
+  async.series([
+    function(callback){ ummon.createTask({"collection":"important","name":"one","command": "echo one" }, callback); },
+    function(callback){ ummon.createTask({"collection":"important","name":"two","command": "echo two"}, callback); },
+    function(callback){ ummon.createTask({"collection":"notimportant","name":"one","command": "echo two"}, callback); },
+    // Now the wildcard tasks
+    function(callback){ ummon.createTask({"collection":"cleanup","name":"important","command": "echo two", "trigger":"important.*" }, callback); },
+    function(callback){ ummon.createTask({"collection":"cleanup","name":"all","command": "echo two", "trigger":"*" }, callback); },
+  ],
+  function(err){
+    t.equal(ummon.getTaskReferences('important.one')[0], 'cleanup.important', 'cleanup.important references important.one as a dependant');
+    t.equal(ummon.getTaskReferences('important.one')[1], 'cleanup.all', 'cleanup.all references important.one as a dependant');
+    t.similar(ummon.getTaskDependencies('cleanup.all'), ['important.one','important.two','notimportant.one', 'cleanup.important'], 'cleanup.all depends on all the tasks');
+  });
+});
