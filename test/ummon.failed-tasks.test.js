7fdb34bc3582d345c512722a265b424ddad37b02
Matt McManus <mattmcmanus@gmail.com>
2013-08-27 16:44:20 -0400

    Dynamic command replacement. Closes #54

--- test/ummon.failed-tasks.test.js
+++ test/ummon.failed-tasks.test.js
@@ -6,12 +6,23 @@ var async = require("async");
 
 var ummon = require('..')({autoSave:false});
 
+var taskRunId;
+
 test('Triggerer proper tasks on failure', function(t){
-  t.plan(11)
+  t.plan(12)
 
   ummon.on('queue.new', function(run){
     t.ok(true, 'The queue.new emitter was emited');
-    t.ok((run.task.name === 'goodbye' || run.task.name === 'runMeOnErrors'), 'The queue is added to goodbye or runMeOnErrors');
+
+    if (run.task.name === 'goodbye') {
+      t.ok(run.task, 'goodbye is added to the queue');
+      taskRunId = run.id;
+    }
+
+    if (run.task.name === 'runMeOnErrors') {
+      t.ok(run.task, 'runMeOnErrors is added to the queue');
+      t.equal(run.task.command, 'echo '+taskRunId+' failed', 'The tasks command is dynamically updated')
+    }
   });
 
   ummon.on('worker.complete', function(run){
@@ -21,12 +32,11 @@ test('Triggerer proper tasks on failure', function(t){
 
   async.series([
     function(callback){ ummon.createTask({"name": "goodbye", "command": "echo goodbye && exit 1"},  callback)},
-    function(callback){ ummon.createTask({ "name": "runMeOnErrors", "command": "echo adios;","trigger": { "afterFailed": 'goodbye' }}, callback) }
+    function(callback){ ummon.createTask({ "name": "runMeOnErrors", "command": "echo <%= run.id %> failed","trigger": { "afterFailed": 'goodbye' }}, callback) }
   ],
   function(err){
     t.ok(ummon.tasks['ummon.goodbye'], 'There is a goodbye task');
     t.ok(ummon.tasks['ummon.runMeOnErrors'], 'There is a runMeOnErrors task');
-    console.log(ummon.dependencies['error'].state)
     t.equal(ummon.getTaskReferences('goodbye', 'error')[0], 'ummon.runMeOnErrors', 'ummon.runMeOnErrors is a dependent task for goodbye');
   });
 
