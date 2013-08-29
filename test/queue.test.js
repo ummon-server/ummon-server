4cdc127382bb3b0784f6cda55ee8f5c94a001ee7
Matt McManus <mattmcmanus@gmail.com>
2013-08-29 16:21:50 -0400

    Fixing "cannot read property 'id' of undefined" crash

--- test/queue.test.js
+++ test/queue.test.js
@@ -2,33 +2,6 @@
 
 var test = require("tap").test;
 
-// var thingie, superEasy;
-
-// test("make sure the thingie is a thing", function (t) {
-//   t.equal(thingie, "thing", "thingie should be thing");
-//   t.type(thingie, "string", "type of thingie is string");
-//   t.ok(true, "this is always true");
-//   t.notOk(false, "this is never true");
-//   t.test("a child test", function (t) {
-//     t.equal(this, superEasy, "right!?");
-//     t.similar(7, 2, "ever notice 7 is kinda like 2?", {todo: true});
-//     t.test("so skippable", {skip: true}, function (t) {
-//       t.plan(1); // only one test in this block
-//       t.ok(true, "but when the flag changes, it'll pass");
-//       // no need to end, since we had a plan.
-//     });
-//     t.end();
-//   });
-//   t.ok(99, "can also skip individual assertions", {skip: true});
-//   // end lets it know it's over.
-//   t.end();
-// });
-// test("another one", function (t) {
-//   t.plan(1);
-//   t.ok(true, "It's ok to plan, and also end.  Watch.");
-//   t.end(); // but it must match the plan!
-// });
-
 var queue = require('../lib/queue.js');
 
 var ummon = { emit: function(name) {} }; // emulate event emitter
@@ -76,3 +49,17 @@ test('Test retreiving the next item from the queue that is not currently running
   t.equal(testQueue.items.length, 2, 'There are now two items in the queue');
   t.equal(item.task.command, 'three', 'The correct item was returned');
 });
+
+test('Make sure an empty queue doesn\'t return nothing', function(t){
+  t.plan(4);
+
+  var item = testQueue.getNext(['two']);
+
+  t.equal(testQueue.items.length, 1, 'Theres now one item in the queue');
+  t.equal(item.task.command, 'four', 'The correct item was returned');
+
+  var item2 = testQueue.getNext(['two']);
+
+  t.equal(testQueue.items.length, 1, 'Theres still one item in the queue');
+  t.equal(item2, false, 'No item was returned');
+})
