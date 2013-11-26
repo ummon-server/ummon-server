46fa1bc1949a7c72b11cf1e35e952c3fc729ff63
Gabriel Farrell <g@grrawr.com>
2013-11-26 15:43:36 -0500

    Updated and deleted task triggers now get stopped
    We need to stop the timer, not just remove the reference. Closes #68
    and punkave/ummon#7. Rookie move, @mattmcmanus. :disappointed:

--- test/ummon.task-update.test.js
+++ test/ummon.task-update.test.js
@@ -2,6 +2,8 @@ var test = require('tap').test;
 var ummon = require('..')({pause:true, autoSave:false});
 
 
+var timer;
+
 test('Create a task', function (t) {
   t.plan(3);
 
@@ -13,12 +15,15 @@ test('Create a task', function (t) {
   }, function (err, task) {
     t.ifError(err, 'No error returned to task creation callback');
     t.equal(task.trigger.time, '* * * * *', 'Task trigger set as it should be');
-    t.equal(ummon.timers['science.nye'].cronTime.source, '* * * * *', 'Timer matches trigger');
+
+    // Grab the timer for checking later
+    timer = ummon.timers['science.nye'];
+    t.equal(timer.cronTime.source, '* * * * *', 'Timer matches trigger');
   });
 });
 
 test('Update a task', function (t) {
-  t.plan(3);
+  t.plan(4);
 
   ummon.updateTask('science.nye', {
     trigger: '5 * * * *'
@@ -26,6 +31,9 @@ test('Update a task', function (t) {
     t.ifError(err, 'No error returned to task update callback');
     t.equal(task.trigger.time, '5 * * * *', 'Task trigger set as it should be');
     t.equal(ummon.timers['science.nye'].cronTime.source, '5 * * * *', 'Timer matches trigger');
+
+    // Check to make sure the timer we set earlier is no longer running
+    t.ok(!timer.running, 'Old timer has been stopped');
   });
 });
 
